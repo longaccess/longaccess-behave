6bf5bf2ea252504b5df2ca713bcfc6d61206b361
Konstantinos Koukopoulos <koukopoulos@gmail.com>
2014-05-19 12:01:52 +0300

    add steps for manipulating S3 keys

--- behave_cli/s3mock/steps.py
+++ behave_cli/s3mock/steps.py
@@ -11,3 +11,73 @@ def s3bucket_named(context, name):
     boto = connect_s3()
     boto.create_bucket(name)
     context.buckets.append(name)
+
+
+@step(u'there are {num} keys in the bucket "{bucket}"')
+def s3bucket_num_keys_bucket(context, num, bucket):
+    bucket = connect_s3().get_bucket(bucket)
+    assert len(bucket.get_all_keys()) == int(num), "{} keys in {}".format(
+        num, bucket)
+
+
+@step(u'there are {num} keys')
+def s3bucket_num_keys(context, num):
+    context.execute_steps(u"""
+        Given there are {} keys in the bucket "{}"
+        """.format(num, context.buckets[0]))
+
+
+@step(u'the key "{key}" exists in the bucket "{bucket}"')
+def s3bucket_key_bucket(context, key, bucket):
+    bucket = connect_s3().get_bucket(bucket)
+    assert bucket.get_key(key) is not None, "key {} exist in {}".format(
+        key, bucket)
+
+
+@step(u'the key "{key}" exists')
+def s3bucket_key(context, key):
+    context.execute_steps(u"""
+        Given the key {} exists in the bucket "{}"
+        """.format(key, context.buckets[0]))
+
+
+@step(u'the following keys exists in the bucket "{bucket}"')
+def s3bucket_keys_bucket(context, bucket):
+    step = u"""
+        Given the key "{}" exists in the bucket "{}"
+    """
+    for row in context.table:
+        context.execute_steps(step.format(row['key'], bucket))
+
+
+@step(u'the following keys exist')
+def s3bucket_keys(context):
+    s3bucket_keys_bucket(context, context.buckets[0])
+
+
+@step(u'I set the contents of key "{key}" in bucket "{b}" to "{contents}"')
+def s3bucket_set_key_bucket(context, key, b, contents):
+    bucket = connect_s3().get_bucket(b)
+    key = bucket.new_key(key)
+    key.set_contents_from_string(contents)
+
+
+@step(u'I set the contents of key "{key}" to "{contents}"')
+def s3bucket_set_key(context, key, contents):
+    context.execute_steps(u"""
+        Given I set the contents of key "{}" in bucket "{}" to "{}"
+        """.format(key, context.buckets[0], contents))
+
+
+@step(u'I set the contents of the following keys in bucket "{b}"')
+def s3bucket_set_keys_bucket(context, b):
+    step = u"""
+        Given I set the contents of key "{}" in bucket "{}" to "{}"
+    """
+    for row in context.table:
+        context.execute_steps(step.format(row['key'], b, row['content']))
+
+
+@step(u'I set the contents of the following keys')
+def s3bucket_set_keys(context):
+    s3bucket_set_keys_bucket(context, context.buckets[0])
